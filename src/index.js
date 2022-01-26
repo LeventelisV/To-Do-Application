4e5ad0dacb3588c6183af20f9ad47bbd837f69a8
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2022-01-26 13:19:15 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-// import App from "./ComponentsSimpleState/App"
-import App from "./ComponentsContextApi/App"
+import App from "./ComponentsSimpleState/App"
+// import App from "./ComponentsContextApi/App"
 // import App from "./ComponentsReactRedux/App"
 
 // import FirstTry from "./FirstTry";
