3194862bfc9f9d740992466b0d639b256bc3e52e
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2022-10-28 16:15:42 +0200

    change ItemStatus return DOM element

--- src/ComponentsSimpleState/ItemStatus.jsx
+++ src/ComponentsSimpleState/ItemStatus.jsx
@@ -1,5 +1,5 @@
 export default function ItemStatus({ items }) {
   const undoneItems = items.filter((item) => !item.done);
 
-  return <div>Your undone tasks are: {undoneItems.length}</div>;
+  return <p>Your undone tasks are: {undoneItems.length}</p>;
 }
---
+++ src/ComponentsSimpleState/tests/ItemStatus.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import items from "../../mock/items.js";
+import ItemStatus from "../ItemStatus.jsx";
+
