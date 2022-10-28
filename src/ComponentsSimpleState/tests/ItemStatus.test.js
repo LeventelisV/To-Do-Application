899d9485602593f807dcc9c8583dd1d9f6b86701
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2022-10-28 16:26:16 +0200

    Test ItemStatus component

--- src/ComponentsSimpleState/tests/ItemStatus.test.js
+++ src/ComponentsSimpleState/tests/ItemStatus.test.js
@@ -4,3 +4,12 @@ import "@testing-library/jest-dom";
 import items from "../../mock/items.js";
 import ItemStatus from "../ItemStatus.jsx";
 
+beforeEach(()=>{
+    render(<ItemStatus items={items}/>)
+})
+
+describe("ItemStatus",()=>{
+    test("element with correct number of undone items should be in the dom",()=>{
+        expect(screen.getByText("Your undone tasks are: 2")).toBeInTheDocument();
+    })
+})
