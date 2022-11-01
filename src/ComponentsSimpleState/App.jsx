f2bb8d35b7813605f9ce8ba2a7711612f59c5f21
LeventelisV <61799850+LeventelisV@users.noreply.github.com>
2022-11-01 14:39:31 +0100

    wip

--- src/ComponentsSimpleState/App.jsx
+++ src/ComponentsSimpleState/App.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import ToDoList from "./ToDoList";
 import UserInput from "./UserInput";
 import ShowItems from "./ShowItems"
+import itemsnew from "../mock/items"
 
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
 
     return (
         <div className="m-16 rounded-lg shadow-xl border min-h-screen  text-center">
+            {console.log("itemsnew",itemsnew)}
             <ShowItems items={items} setItems={setItems}/>
             <UserInput items={items} setItems={setItems}/>
             <ToDoList items={items} setItems={setItems}/>
---
+++ src/ComponentsSimpleState/tests/ToDoItem.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import ToDoItem from "../ToDoItem";
+import {items} from "../../mock/items.js"
+
+// const item = items[0]
+// beforeEach(()=>{
+//     const setItemsMockFn = jest.fn();
+//     render (<ToDoItem setItems={setItemsMockFn} setTaskRender={setItemsMockFn}  item={item}/>)
+// })
+
+describe("ToDoItem",()=>{
+    test("the name of the item should be displayed",()=>{
+
+    })
+
+})
